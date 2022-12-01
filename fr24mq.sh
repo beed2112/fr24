4aa72eac84ab01cb0ad0ebef16c637e9a1bf55ff
beed2112 <32287825+beed2112@users.noreply.github.com>
2022-11-30 19:44:34 -0700

    Update fr24mq.sh

--- fr24mq.sh
+++ fr24mq.sh
@@ -1,9 +1,81 @@
-desc=`grep -i $1 callSignDescription | awk -F @ '{print $2}'`
+an desc=`grep -i $1 ./callSignDescription | awk -F @ '{print $2}'`
 
 if [$desc == ""]
  then
     desc="unknown aircraft"
 fi
-    
+
+STR='GNU/Linux is an operating system'
+SUB='Linux'
+
+case $1 in
+
+  *"N507MP"*)
+    desc="the Mesa Police"
+    ;;
+
+  *"COPPER"*)
+    desc="an Air Force Tanker"
+    ;;
+
+  *"MADCAT"*)
+    desc="an Air Force Training Jet"
+    ;;
+
+  *"N12718 "*)
+    desc="an Old Silver prop plane"
+    ;;
+
+   *"S211"*)
+    desc="an Italian Fighter"
+    ;;
+
+  *"N16BP"*)
+    desc="a Bi plane"
+    ;;
+
+  *"MADCAT"*)
+    desc="an Air Force Training Jet"
+    ;;
+
+  *"KYOTE"*)
+    desc="an Army Black Hawk"
+    ;;
+
+  *"RAIDR"*)
+    desc="a Navy Tanker"
+    ;;
+
+  *"ZAPER"*)
+    desc="an Air Force Tanker"
+    ;;
+
+  *"TRON"*)
+    desc="an Air Force Tanker"
+    ;;
+
+  *"N55IAM"*)
+    desc="an Air Force Tanker"
+    ;;
+
+  *"F16"*)
+    desc="an Air Force F sixteen"
+    ;;
+
+  *"TNKR"*)
+    desc="a Fire fighting tanker"
+    ;;
+
+  *"SNTRY"*)
+    desc="a Boeing Sentry RADAR"
+    ;;
+
+  *"OXF"*)
+    desc="a Flight School plane"
+    ;;
+esac
+
+
 mosquitto_pub -h 192.168.0.253  -t planes/watchfor -u me -P me -m "$1, $desc"
+
 
