0366ba6313de10264df4cfa0c5cece6d86c28d91
beed2112 <32287825+beed2112@users.noreply.github.com>
2022-12-02 19:20:00 -0700

    Add files via upload

--- fr24mq.sh
+++ fr24mq.sh
@@ -1,97 +1,129 @@
 #desc=`grep -i $1 ./callSignDescription | awk -F @ '{print $2}'`
 
-if [$desc == ""]
- then
-    desc="unknown aircraft"
-fi
+DUMP1090IP=`grep DUMP1090IP fr24mq.confg | awk -F= '{print $2}'`
+DUMP1090PORT=`grep DUMP1090PORT fr24mq.confg | awk -F= '{print $2}'`
+MQTTIP=`grep MQTTIP fr24mq.confg | awk -F= '{print $2}'`
+MQTTPORT=`grep MQTTPORT fr24mq.confg | awk -F= '{print $2}'`
+MQTTTOPIC=`grep MQTTTOPIC fr24mq.confg | awk -F= '{print $2}'`
+CALLSIGNDESCRIPTION=`grep CALLSIGNDESCRIPTION fr24mq.confg | awk -F= '{print $2}'`
 
-STR='GNU/Linux is an operating system'
-SUB='Linux'
- echo $1
-case $1 in
-
-  *"N507MP"*)
-    desc="a Mesa Police Helicoptor"
-    ;;
-
-  *"N505MP"*)
-    desc="a Mesa Police Helicoptor"
-    ;;
-
-  *"COPPER"*)
-    desc="an Air Force Tanker"
-    ;;
-
-  *"MADCAT"*)
-    desc="an Air Force Training Jet"
-    ;;
-
-  *"N12718"*)
-    desc="an Old Silver prop plane"
-    ;;
-
-   *"S211"*)
-    desc="an Italian Fighter"
-    ;;
-
-  *"N16BP"*)
-    desc="a Bi plane"
-    ;;
-
-  *"MADCAT"*)
-    desc="an Air Force Training Jet"
-    ;;
-
-  *"KYOTE"*)
-    desc="an Army Black Hawk"
-    ;;
-
-  *"RAIDR"*)
-    desc="a Navy Tanker"
-    ;;
-
-  *"ZAPER"*)
-    desc="an Air Force Tanker"
-    ;;
-
-  *"TRON"*)
-    desc="an Air Force Tanker"
-    ;;
-
-  *"N55IAM"*)
-    desc="an Air Force Tanker"
-    ;;
-
-  "F16"*)
-    desc="an Air Force F sixteen"
-    ;;
-
-  *"TNKR"*)
-    desc="a Fire fighting tanker"
-    ;;
-
-  *"SNTRY"*)
-    desc="a Boeing Sentry RADAR"
-    ;;
-
-  "OXF"*)
-    desc="a Flight School plane"
-    ;;
+ echo "$1"
 
 
-  "HGT"*)
-    desc="an Intel Air Shuttle"
-    ;;
+inplane=$1 
+echo "$DUMP1090IP"
+echo "$DUMP1090PORT"
+echo "$MQTTIP"
+echo "$MQTTPORT"
+echo "$MQTTTOPIC"
+echo "$CALLSIGNDESCRIPTION"
 
-  "DOJ"*)
-    desc="a US Marshall, Con Air flight "
-    ;;
-    *)
-    desc="an unknown aircraft"
-    ;;
-esac
+descout="unknown aircraft"
+
+for foo in `awk -F@ '{print $1"@"$2}' /fr24/callSignDescription ` 
+do 
+  echo "in for loop"
+ res=`echo $foo | sed 's/ /-/g'`
+ call=`echo $foo | awk -F@ '{print $1}'`
+ desc=`echo $foo | awk -F@ '{print $2}' | sed 's/-/ /g'`
+#echo "$inplane    $call"
+ #echo " $desc      $call"
+ 
+ 
+ if [[ "$inplane"  =~ .*"$call"*. ]]; then 
+    descout="$desc"
+ fi 
+
+done
 
 
-mosquitto_pub -h 192.168.0.253  -t planes/watchfor -u me -P me -m "$1, $desc"
+echo "output $inplane    $descout"
+#mosquitto_pub -h 192.168.0.253  -t planes/watchfor -u me -P me -m "$inplane, $descout"
+
+
+# case $1 inS
+
+#   *"N507MP"*)
+#     desc="a Mesa Police Helicoptor"
+#     ;;
+
+#   *"N505MP"*)
+#     desc="a Mesa Police Helicoptor"
+#     ;;
+
+#   *"COPPER"*)
+#     desc="an Air Force Tanker"
+#     ;;
+
+#   *"MADCAT"*)
+#     desc="an Air Force Training Jet"
+#     ;;
+
+#   *"N12718"*)
+#     desc="an Old Silver prop plane"
+#     ;;
+
+#    *"S211"*)
+#     desc="an Italian Fighter"
+#     ;;
+
+#   *"N16BP"*)
+#     desc="a Bi plane"
+#     ;;
+
+#   *"MADCAT"*)
+#     desc="an Air Force Training Jet"
+#     ;;
+
+#   *"KYOTE"*)
+#     desc="an Army Black Hawk"
+#     ;;
+
+#   *"RAIDR"*)
+#     desc="a Navy Tanker"
+#     ;;
+
+#   *"ZAPER"*)
+#     desc="an Air Force Tanker"
+#     ;;
+
+#   *"TRON"*)
+#     desc="an Air Force Tanker"
+#     ;;
+
+#   *"N55IAM"*)
+#     desc="an Air Force Tanker"
+#     ;;
+
+#   "F16"*)
+#     desc="an Air Force F sixteen"
+#     ;;
+
+#   *"TNKR"*)
+#     desc="a Fire fighting tanker"
+#     ;;
+
+#   *"SNTRY"*)
+#     desc="a Boeing Sentry RADAR"
+#     ;;
+
+#   "OXF"*)
+#     desc="a Flight School plane"
+#     ;;
+
+
+#   "HGT"*)
+#     desc="an Intel Air Shuttle"
+#     ;;
+
+#   "DOJ"*)
+#     desc="a US Marshall, Con Air flight "
+#     ;;
+#     *)
+#     desc="an unknown aircraft"
+#     ;;
+# esac
+
+
 
 
