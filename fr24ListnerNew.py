a32dc8830f60272c8fa458b8e20f4b78e9a94841
beed2112 <32287825+beed2112@users.noreply.github.com>
2023-08-07 16:22:47 -0700

    Update fr24ListnerNew.py

--- fr24ListnerNew.py
+++ fr24ListnerNew.py
@@ -538,7 +538,7 @@ while True:
   outline =  part1 + part2 +  "--" + part4 + "--" + part5 +  "--"  + part6
   print (outline) 
 
-   
+  outcolor =  "cyan" 
   mqttOutLine = str(outcolor) + "|" +  part1 + part2 
   cmd = 'mosquitto_pub -h 192.168.0.253  -t planes/console -u me -P me -m "'  + mqttOutLine +'"'
   os.system(cmd) 
